455663aa8a39ee48c253de53540b57c0abd3f57e
Sadiq Teslim <sadiqadetola08@gmail.com>
2025-05-30 16:07:29 +0100

    feat: landing page feat: chat prompt page feat: tailwindcss configuration

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./*.html", "./src/**/*.{js,jsx,ts,tsx}" /* any other paths */],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'Noto Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'), // For the 'prose' class in Verifact messages
+  ],
+}
